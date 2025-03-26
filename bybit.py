7395c1e6daac8bdbed5ecbebd23374be9b583536
haringuyendev <hainguyenhoang1502@gmail.com>
2025-03-26 10:19:16 +0700

    change keys

--- bybit.py
+++ bybit.py
@@ -1,7 +1,7 @@
-api="fBPmx14YAuIODJgs7s"
-secret="Mi5JIBwohAkHhf8hGebSJ7czY66MpEUquWCA"
-bot_token="8059122655:AAFtt2kZ_VoubaI2rMGxWyfloiN_tZxeOOY"
-chat_id="-1002605501999"
+api="XXXXXXXX"
+secret="XXXXXXXXXXX"
+bot_token="XXXXXXXXXXX"
+chat_id="XXXXXXXXXXX"
 # Bybit WebSocket URL
 # Nếu chạy tài khoản thực thì bật dòng ở dưới, comment dòng thứ 2 lại
 BYBIT_WS_URL = "wss://stream.bybit.com/v5/public/linear" 
